4a84080e908f12413fd5da1698f85575f3c0d54c
zhangyue <zyue_3@163.com>
2022-08-29 15:49:20 +0800

    1111

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,25 @@
 <template>
-    <div></div>
+    <div>
+        <slotVue>
+            <template v-slot:header>
+                <h1>Here might be a page title</h1>
+            </template>
+            <template v-slot:default>
+                <p>A paragraph for the main content.</p>
+            </template>
+            <template v-slot:footer>
+                <p>Here's some contact info</p>
+            </template>
+        </slotVue>
+    </div>
 </template>
 
 <script>
-
+import slotVue from './slot.vue'
 export default {
   name: 'HomeView',
   components: {
+    slotVue
   }
 }
 </script>
--- src/views/slot.vue
+++ src/views/slot.vue
@@ -1,12 +1,14 @@
 /*
  * @Author: zhangyue
  * @Date: 2022-08-29 15:06:42
- * @Last Modified by:   zhangyue
- * @Last Modified time: 2022-08-29 15:06:42
+ * @Last Modified by: zhangyue
+ * @Last Modified time: 2022-08-29 15:39:57
  */
 <template>
   <div>
-
+    <slot name="header"></slot>
+    <slot name="default"></slot>
+    <slot name="footer"></slot>
   </div>
 </template>
 
