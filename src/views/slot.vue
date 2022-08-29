5efe39613f2b62100b0f96af942661c8626d2ab8
zhangyue <zyue_3@163.com>
2022-08-29 15:07:01 +0800

    111

---
+++ src/views/slot.vue
@@ -0,0 +1,34 @@
+/*
+ * @Author: zhangyue
+ * @Date: 2022-08-29 15:06:42
+ * @Last Modified by:   zhangyue
+ * @Last Modified time: 2022-08-29 15:06:42
+ */
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    }
+  },
+  components () {
+  },
+  created () {
+  },
+  mountend () {
+  },
+  methods: {
+  },
+  watch: {
+  }
+}
+</script>
+
+<style  scoped>
+
+</style>
